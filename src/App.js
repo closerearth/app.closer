885871dafb9b96c8e9e64d07a487638be8155ebc
samueldelesque <hello@samueldelesque.me>
2021-12-21 00:31:53 +0100

    Initialize with tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/tailwind.css';
+import Card from './components/card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        category="foo"
+        title="I like bar"
+        content="Some lonnnnnngggg text"
+        // photo="/logo192.png"
+      />
     </div>
   );
 }
