27f64fd035c34b3d55d6cad52e2e6210b8de88c3
samueldelesque <hello@samueldelesque.me>
2021-12-21 00:41:28 +0100

    Add base branding

--- src/components/card.js
+++ src/components/card.js
@@ -4,18 +4,18 @@ const Card = ({
   content,
   photo
 }) => (
-  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  <div class="md:flex">
-    { photo && <div class="md:shrink-0">
-      <img class="h-48 w-full object-cover md:h-full md:w-48" src={ photo } alt="Man looking at item at a store" />
-    </div> }
-    <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{ category }</div>
-      <h3 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{ title }</h3>
-      { content && <p class="mt-2 text-gray-500">{ content }</p> }
+  <div class="max-w-md mx-auto bg-white rounded-md shadow-lg overflow-hidden md:max-w-1xl">
+    <div class="md:flex">
+      { photo && <div class="md:shrink-0">
+        <img class="h-48 w-full object-cover md:h-full md:w-48" src={ photo } alt="Man looking at item at a store" />
+      </div> }
+      <div class="p-8">
+        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{ category }</div>
+        <h3 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{ title }</h3>
+        { content && <p class="mt-2 text-gray-500">{ content }</p> }
+      </div>
     </div>
   </div>
-</div>
 );
 
 export default Card;
