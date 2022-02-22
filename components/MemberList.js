6530da5855ec723d1504f592e84d84f558278322
Francisco Alderete <franalderete7@gmail.com>
2022-02-22 17:39:08 -0300

    members list

--- components/MemberList.js
+++ components/MemberList.js
@@ -50,20 +50,31 @@ const MemberList = ({
   }, [filter, channel, page]);
 
   return (
-    <section className={card ? 'card' : ''}>
-      { title && <h3 className={card ? 'card-title' : ''}>
-        { title }
-      </h3> }
+    <section className="member-page">
+      <Link href={"/"}>
+      <button className='flex flex-row justify-evenly items-center w-28 h-6 self-start rounded-full border border-neutral-900 text-xs'>
+      <FontAwesomeIcon icon={faLongArrowAltLeft} className='w-10/12' />
+         go back
+      </button>
+      </Link>
+      <h3 className="mt-4 mb-12 text-4xl font-light">
+        { title || 'Community members' }
+      </h3>
       { loading ?
         <Loading />:
-        <div className={`user-list ${card?'card-body':''} flex ${list?'flex-col':'flex-row flex-wrap'} justify-start`}>
+        <div className="grid gap-4 md:grid-cols-2 justify-start items-center">
           { users && users.count() > 0 ?
             users.map(user => (
               <Link key={ user.get('_id') } as={`/members/${user.get('slug')}`} href="/members/[slug]">
-                <a className={`user-preview flex flex-row justify-start items-center mb-2 p-2 ${list?'':'w-1/2 md:w-1/5'}`}>
-                  <ProfilePhoto user={user.toJS()} size="lg" />
-                  <h4 className="font-sm ml-2">{ user.get('screenname') }</h4>
-                </a>
+                <div className="flex flex-col justify-start items-center p-5 w-12/12 max-w-md md:max-w-xl md:w-11/12 border border-zinc-400 rounded-sm">
+                  <div className='flex flex-row items-center justify-between w-full'>
+                  <h4 className="font-light text-2xl md:text-2xl">{ user.get('screenname') }</h4>
+                  <ProfilePhoto user={user.toJS()} width={"12"} height={"12"}/>
+                  </div>
+                  <p className='mb-3 w-10/12 self-start text-zinc-400 text-sm'>Lorem ipsum dolor sit amet, consectetur adipscing elit, sed do eiusmod tempor incididunt t labore et dolore.</p>
+                  <h4 className="text-xs flex self-start mb-3">{user.get('timezone')}</h4>
+                  <button className='w-full md:w-52 self-start rounded-full border border-neutral-900 h-9'>See profile</button>
+                </div>
               </Link>
             )):
             <p>No members.</p>
