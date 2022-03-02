68d46c74245e241354a31024503904961e929364
Closer Earth <closer.earth@protonmail.com>
2022-03-02 09:16:34 -0600

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,35 +9,24 @@ const footer = () => (
   <div className=" border-t border-t-line">
     <footer className="main-content flex flex-col items-center p-4">
         <div className="flex flex-col md:flex-row py-2 items-center w-full justify-between">
-          <div className='flex flex-col items-center'>
-          <div className="flex flex-row space-x-5 self-center p-4 mb-8 md:mb-1">
-            { INSTAGRAM_URL && <a href={ INSTAGRAM_URL } target="_blank" rel="noreferrer nofollow">
-              <img src="/images/icons/instagram.svg" width="40" alt="instagram" className="bg-primary rounded-3xl p-1"/>
-            </a> }
-            {FACEBOOK_URL && <a href={ FACEBOOK_URL } target="_blank" rel="noreferrer nofollow">
-              <img src="/images/icons/facebook.svg" width="40" alt="facebook" className="bg-primary rounded-3xl p-1" />
-            </a> }
-            { TWITTER_URL && <a href={ TWITTER_URL } target="_blank" rel="noreferrer nofollow">
-              <img src="/images/icons/twitter.svg" width="40" alt="twitter" className="bg-primary rounded-3xl p-1" />
-            </a> }
-            { DISCORD_URL && <a href={ DISCORD_URL } target="_blank" rel="noreferrer nofollow">
-              <img src="/images/icons/discord.svg" width="40" alt="twitter" className="bg-primary rounded-3xl p-1"/>
-            </a> }
-          </div>
-          <div className='flex flex-row items-center'>
-          <p className='p-2'>Powered by:</p>
-          <h3 className="logo">
-            <Link href="/">
-              <a className="block">
-                { LOGO_HEADER ? <img
-                  src={LOGO_HEADER}
-                  alt={PLATFORM_NAME}
-                /> : PLATFORM_NAME }
-              </a>
-            </Link>
-          </h3>
-          <p className='p-2'>Closer:</p>
-          </div>
+          <div className='flex flex-col items-start'>
+            <div className="flex flex-row mb-8 md:mb-1">
+              { INSTAGRAM_URL && <a href={ INSTAGRAM_URL } target="_blank" rel="noreferrer nofollow">
+                <img src="/images/icons/instagram.svg" width="40" alt="instagram" className="bg-primary rounded-3xl p-1 mr-2"/>
+              </a> }
+              {FACEBOOK_URL && <a href={ FACEBOOK_URL } target="_blank" rel="noreferrer nofollow">
+                <img src="/images/icons/facebook.svg" width="40" alt="facebook" className="bg-primary rounded-3xl p-1 mr-2" />
+              </a> }
+              { TWITTER_URL && <a href={ TWITTER_URL } target="_blank" rel="noreferrer nofollow">
+                <img src="/images/icons/twitter.svg" width="40" alt="twitter" className="bg-primary rounded-3xl p-1 mr-2" />
+              </a> }
+              { DISCORD_URL && <a href={ DISCORD_URL } target="_blank" rel="noreferrer nofollow">
+                <img src="/images/icons/discord.svg" width="40" alt="twitter" className="bg-primary rounded-3xl p-1 mr-2"/>
+              </a> }
+            </div>
+            <div className='flex flex-col items-start mt-8'>
+              <p className="text-xs">Made with ❤️ by <a href="https://closer.earth">Closer</a></p>
+            </div>
           </div>
 
          <Newsletter placement="Footer" />
