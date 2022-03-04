893ce7893f09336930bee76565cd0152c89ce8bd
Francisco Alderete <franalderete7@gmail.com>
2022-03-04 16:08:55 -0300

    add featured sessions mock-up

--- pages/events/[slug]/index.js
+++ pages/events/[slug]/index.js
@@ -260,7 +260,7 @@ const Event = ({ event, error }) => {
                     <div className="flex flex-col w-96 space-y-1 items-start justify-start">
                       <h3 className="font-medium">Speaker Name</h3>
                       <h5 className="text-sm font-medium">Website</h5>
-                      <p className="flex w-72 text-xs"> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      <p className="flex w-84 text-xs"> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna
                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                         ullamco laboris nisi ut aliquip ex ea commodo consequat. 
@@ -271,6 +271,33 @@ const Event = ({ event, error }) => {
                   </div>
                   
                   <button className="btn-primary w-52 mt-8">See more speakers</button>
+                </div>
+              </div>
+
+              <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row items-start justify-start w-full md:ml-full md:mr-full pb-12 pt-12 border-b border-black" >
+                <div className="flex flex-col w-full md:w-5/12">
+                  <h4 className="text-xl font-light">Featured Sessions</h4>
+                </div>
+                <div className="flex flex-col md:flex-row w-full md:w-7/12">
+                  <div className="flex flex-row items-start justify-start w-full md:w-1/2" >
+                   <div className="flex flex-col w-full ">
+                    <img src="/images/sea.jpg" alt="avatar" className="flex w-full md:w-11/12 h-auto"/>
+                    <div className="flex flex-row items-center justify-start mt-4">
+                     <img src="/images/icons/avatar.jpg" alt="avatar" className="w-10"/>
+                     <p className="font-semibold">Speaker Name</p>
+                    </div>
+                   </div>
+                  </div>
+                  <div className="flex flex-col w-full mt-5 md:mt-0 md:w-1/2 space-y-1 items-start justify-start">
+                      <h3 className="font-semibold text-lg">Lorem ipsum dolr sit amet? labore dolore magna 2022</h3>
+                      <p className="flex w-full text-lg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. 
+                        </p>
+                      <p className="text-sm font-semibold">Mar 1, 2022 - 14h</p>
+                      <p className="text-sm font-semibold">Online</p>
+                    </div>
+
                 </div>
               </div>
 
