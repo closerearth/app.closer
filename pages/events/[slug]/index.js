67ab91b86da1564f090530bbf6fdbfabf957335d
Francisco Alderete <franalderete7@gmail.com>
2022-03-07 15:39:01 -0300

    styled add partner form as a modal dialog

--- pages/events/[slug]/index.js
+++ pages/events/[slug]/index.js
@@ -19,6 +19,9 @@ import TimeSince from "../../../components/TimeSince";
 import PageNotFound from "../../404";
 import { useAuth } from "../../../contexts/auth";
 import { usePlatform } from "../../../contexts/platform";
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 dayjs.extend(advancedFormat);
 
@@ -31,6 +34,7 @@ const Event = ({ event, error }) => {
   const [loadError, setErrors] = useState(null);
   const [password, setPassword] = useState("");
   const [featured, setFeatured] = useState(event && !!event.featured);
+  const [partnerForm, togglePartnerForm] = useState(false)
   const { platform } = usePlatform();
   const { user, isAuthenticated } = useAuth();
   const [attendees, setAttendees] = useState(event && (event.attendees || []));
@@ -223,7 +227,7 @@ const Event = ({ event, error }) => {
                   <h4 className="text-xl font-light">Partners</h4>
                 </div>
                
-                <div className="flex w-full md:w-7/12">
+                <div className="flex flex-col w-full md:w-7/12">
                   <div className="flex flex-row flex-wrap w-full">
                   {event.partners &&
                     event.partners.map(
@@ -244,7 +248,44 @@ const Event = ({ event, error }) => {
                           </a>
                         )
                     )}
-                </div>
+                    </div>
+                  <button className="btn-primary h-fit self-start" onClick={() => togglePartnerForm(!partnerForm)}>Add Partner</button>
+                  { (isAuthenticated && user._id === event.createdBy && partnerForm) &&
+                  <>
+                   <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline">
+                    <div className="relative w-11/12 my-6 mx-auto max-w-3xl">
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col space-y-1 w-full bg-background outline-none focus:outline-none p-10">
+                    <div className="flex flex-row items-center justify-between w-full">
+                    <h3>Add partner</h3>
+                    <FontAwesomeIcon icon={faTimes} className="hover:cursor-pointer" onClick={() => togglePartnerForm(!partnerForm)} />
+                    </div>  
+                    <form className="flex flex-col" onSubmit={ e => addPartner(e, partnerToAdd) }>
+                      <div className="flex flex-col w-full mt-5">
+                        <label>Partner</label>
+                        <input
+                          type="text"
+                          value={ partnerToAdd.name }
+                          placeholder="Partner Name"
+                          onChange={ e => setPartnerToAdd({ ...partnerToAdd, name: e.target.value }) }
+                        />
+                      </div>
+                      <div className="flex flex-col mt-5 w-full">
+                        <div className="flex flex-col items-center justify-start">
+                          <label className="self-start">Logo</label>
+                          { partnerToAdd.photo && <Photo id={ partnerToAdd.photo } className="self-start" /> }
+                          <UploadPhoto
+                            onSave={ photo => setPartnerToAdd({ ...partnerToAdd, photo }) }
+                            label="Upload logo"
+                          />
+                        </div>
+                          <button className="btn-primary mt-5">Add</button>
+                      </div>
+                    </form>
+                  </div>
+                  </div>
+                  </div>
+                  </>
+                   }
                 </div>
               </div>
 
