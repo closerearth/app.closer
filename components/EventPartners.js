0cb7fe2aad84cff39ab468023aaba43cd1d43cf7
Francisco Alderete <franalderete7@gmail.com>
2022-03-28 12:11:33 -0300

    create EventPartners component without error

---
+++ components/EventPartners.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Photo from './Photo';
+
+const EventPartners = ({ event }) => {
+  return (<section className="mb-6">
+    <div className="flex flex-row flex-wrap justify-center items-center">
+      {event.partners && event.partners.map(partner => partner.photoUrl && <a href={partner.url || '#'} target="_blank" rel="noreferrer" key={partner.name} className="mr-3">
+        <Photo id={partner.photo} photoUrl={partner.photoUrl} className="w-32 h-16" title={partner.name} />
+      </a>)}
+    </div>
+    {
+    /* { (isAuthenticated && user._id === event.createdBy) &&
+     <div className="m-4">
+       <h3>Add partner</h3>
+       <form className="flex flex-row p-2" onSubmit={ e => addPartner(e, partnerToAdd) }>
+         <div className="w-2/3">
+           <input
+             type="text"
+             value={ partnerToAdd.name }
+             placeholder="Partner Name"
+             onChange={ e => setPartnerToAdd({ ...partnerToAdd, name: e.target.value }) }
+           />
+         </div>
+         <div className="w-1/3">
+           { partnerToAdd.photo && <Photo id={ partnerToAdd.photo } /> }
+           <div className="flex flex-row``">
+             <UploadPhoto
+               onSave={ photo => setPartnerToAdd({ ...partnerToAdd, photo }) }
+               label="Upload logo"
+             />
+             <button className="btn-primary">Add</button>
+           </div>
+         </div>
+       </form>
+     </div>
+    } */
+    }
+  </section>);
+}
+  
+export default EventPartners;
+  
