b19fc0d49f29b9d5a383c030c4b2fc76c96fcfb4
Francisco Alderete <franalderete7@gmail.com>
2022-05-03 10:41:22 -0300

    add Tabs component to BookingListPreview: open, approved & rejected

--- components/BookingListPreview.js
+++ components/BookingListPreview.js
@@ -1,34 +1,72 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import Link from 'next/link';
 import dayjs from 'dayjs';
+import { usePlatform } from '../contexts/platform';
+import { useAuth } from '../contexts/auth';
 
 import { __, priceFormat } from '../utils/helpers';
 
-const ListingListPreview = ({ booking }) => {
-  if (!booking) {
-    return null;
-  }
+const ListingListPreview = ({ status, limit }) => {
 
-  const start = dayjs(booking.get('start'));
-  const end = dayjs(booking.get('end'));
+  const { user } = useAuth();
+  const { platform } = usePlatform();
+  const [page, setPage] = useState(1);
+  const [error, setErrors] = useState(false);
+  const filter = useMemo(() => ({
+    where: { status },
+    limit,
+    page
+  }), [status, limit, page]);
 
+  const bookings = platform.booking.find(filter);
+
+
+  const loadData = async () => {
+    try {
+      platform.booking.getCount(filter);
+      platform.booking.get(filter);
+    } catch (err) {
+      console.log('Load error', err);
+      setErrors(err.message)
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [platform, filter]);
+
+  
   return (
-    <div className="booking-list-preview card">
-      <p>{ __('bookings_status') } <b>{booking.get('status')}</b></p>
-      <p>{ __('bookings_checkin') } <b>{start.format('LLL')}</b></p>
-      <p>{ __('bookings_checkout') } <b>{end.format('LLL')}</b></p>
-      <p>{ __('bookings_total') }
-        <b className={ booking.get('volunteer') ? 'line-through': '' }>
-          {' '}{priceFormat(booking.get('price'))}
-        </b>
-        <b>{' '}{booking.get('volunteer') && priceFormat(0, booking.getIn(['price', 'cur']))}</b>
-      </p>
-      <p>{ __('bookings_id') } <b>{booking.get('_id')}</b></p>
-      { booking.get('description') &&
+    <div className="application-list grid gap-4">
+      { bookings && bookings.count() > 0 ?
+        bookings.map(book => {
+          const booking = platform.booking.findOne(book.get('_id'));
+          const start = dayjs(booking.get('start'));
+          const end = dayjs(booking.get('end'));
+
+          return (
+            <div key={ booking.get('_id') } className="booking-list-preview card">
+              <p>{ __('bookings_status') } <b>{booking.get('status')}</b></p>
+              <p>{ __('bookings_checkin') } <b>{start.format('LLL')}</b></p>
+              <p>{ __('bookings_checkout') } <b>{end.format('LLL')}</b></p>
+              <p>{ __('bookings_total') }
+                <b className={ booking.get('volunteer') ? 'line-through': '' }>
+                  {' '}{priceFormat(booking.get('price'))}
+                </b>
+                <b>{' '}{booking.get('volunteer') && priceFormat(0, booking.getIn(['price', 'cur']))}</b>
+              </p>
+              <p>{ __('bookings_id') } <b>{booking.get('_id')}</b></p>
+              { booking.get('description') &&
         <p>{ booking.get('description').slice(0, 120) }{ booking.get('description').length > 120 && '...' }</p>
+              }
+            </div>
+          )
+        }):
+        <p className="py-4">No bookings.</p>
       }
+
     </div>
-  );
-}
+  )
+};
 
 export default ListingListPreview;
--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -6,43 +6,27 @@ import Layout from '../../components/Layout';
 import { useAuth } from '../../contexts/auth';
 import { usePlatform } from '../../contexts/platform';
 import BookingListPreview from '../../components/BookingListPreview';
+import Tabs from '../../components/Tabs';
 import { __ } from '../../utils/helpers';
 import PageNotFound from '../404';
 
 const Bookings = () => {
   const router = useRouter();
-
   const { user } = useAuth();
   const { platform } = usePlatform();
-  const bookingFilter = user && { where: { createdBy: user._id } };
-
-  const loadData = async () => {
-    await Promise.all([
-      platform.booking.get(bookingFilter)
-    ]);
-  }
-
-  useEffect(() => {
-    if (user && user._id){
-      loadData();
-    }
-  }, [user]);
+  const [status, setStatus] = useState('open');
 
   if (!user) {
     return <PageNotFound error="User not logged in." />;
   }
 
-  const bookings = platform.booking.find(bookingFilter);
-  console.log('bookings', bookings);
 
   return (
     <Layout>
       <Head>
         <title>{ __('bookings_title') }</title>
       </Head>
-      { bookings && bookings.get('error') &&
-        <div className="validation-error">{ bookings.get('error') }</div>
-      }
+
       <div className="main-content intro fullwidth">
         <div className="columns">
           <div className="col lg two-third">
@@ -50,10 +34,32 @@ const Bookings = () => {
               <h1>{ __('bookings_title') }</h1>
             </div>
             <div className="bookings-list">
-              { bookings && bookings.count() > 0 ?
-                bookings.map(booking => <BookingListPreview key={ booking.get('_id') } booking={ booking } />):
-                'No Bookings'
-              }
+              <Tabs
+                tabs={[
+                  {
+                    title: 'Open',
+                    value: 'open',
+                    content: (
+                      <BookingListPreview status="open" />
+                    )
+                  },
+                  {
+                    title: 'Approved',
+                    value: 'approved',
+                    content: (
+                      <BookingListPreview status="conversation" />
+                    )
+                  },
+                  {
+                    title: 'Rejected',
+                    value: 'rejected',
+                    content: (
+                      <BookingListPreview status="rejected" />
+                    )
+                  },
+                ]}
+                onChange={ tab => setStatus(tab.value) }
+              />
             </div>
           </div>
         </div>
