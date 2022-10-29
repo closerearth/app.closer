c65923afcf06bbbda72aa06fb5366c183612ded0
Vladislav Sorokin <sorokinvj@gmail.com>
2022-10-29 19:04:05 +0100

    add Cancel button

--- components/BookingListPreview.js
+++ components/BookingListPreview.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-
+import Link from 'next/link';
 import dayjs from 'dayjs';
 
 import { __, priceFormat } from '../utils/helpers';
@@ -13,40 +13,27 @@ const ListingListPreview = ({ booking }) => {
   const start = dayjs(booking.get('start'));
   const end = dayjs(booking.get('end'));
 
+  const cancelBooking = () => {
+    console.log('Cancelling booking...', booking.get('_id'));
+  };
+
   return (
     <div className="booking-list-preview card">
-      <p>
-        {__('bookings_status')} <b>{booking.get('status')}</b>
-      </p>
-      <p>
-        {__('bookings_checkin')} <b>{start.format('LLL')}</b>
-      </p>
-      <p>
-        {__('bookings_checkout')} <b>{end.format('LLL')}</b>
-      </p>
-      <p>
-        {__('bookings_total')}
-        <b className={booking.get('volunteer') ? 'line-through' : ''}>
-          {' '}
-          {priceFormat(booking.get('price'))}
+      <p>{ __('bookings_status') } <b>{booking.get('status')}</b></p>
+      <p>{ __('bookings_checkin') } <b>{start.format('LLL')}</b></p>
+      <p>{ __('bookings_checkout') } <b>{end.format('LLL')}</b></p>
+      <p>{ __('bookings_total') }
+        <b className={ booking.get('volunteer') ? 'line-through': '' }>
+          {' '}{priceFormat(booking.get('price'))}
         </b>
-        <b>
-          {' '}
-          {booking.get('volunteer') &&
-            priceFormat(0, booking.getIn(['price', 'cur']))}
-        </b>
+        <b>{' '}{booking.get('volunteer') && priceFormat(0, booking.getIn(['price', 'cur']))}</b>
       </p>
-      <p>
-        {__('bookings_id')} <b>{booking.get('_id')}</b>
-      </p>
-      {booking.get('description') && (
-        <p>
-          {booking.get('description').slice(0, 120)}
-          {booking.get('description').length > 120 && '...'}
-        </p>
-      )}
+      <p>{ __('bookings_id') } <b>{booking.get('_id')}</b></p>
+      { booking.get('description') &&
+        <p>{ booking.get('description').slice(0, 120) }{ booking.get('description').length > 120 && '...' }</p>
+      }
     </div>
   );
-};
+}
 
 export default ListingListPreview;
