876f77dcdc11cdafc87369f48473458b85cba804
Vladislav Sorokin <sorokinvj@gmail.com>
2022-10-30 18:53:27 +0000

    add cancel button dict string and href

--- components/BookingListPreview.js
+++ components/BookingListPreview.js
@@ -1,22 +1,13 @@
 
 import React from 'react';
-import Link from 'next/link';
+import Link from 'next/link'
 import dayjs from 'dayjs';
-
 import { __, priceFormat } from '../utils/helpers';
 
-const ListingListPreview = ({ booking }) => {
-  if (!booking) {
-    return null;
-  }
-
+const BookingListPreview = ({ booking }) => {
   const start = dayjs(booking.get('start'));
   const end = dayjs(booking.get('end'));
-
-  const cancelBooking = () => {
-    console.log('Cancelling booking...', booking.get('_id'));
-  };
-
+  const bookingId = booking.get('_id')
   return (
     <div className="booking-list-preview card">
       <p>{ __('bookings_status') } <b>{booking.get('status')}</b></p>
@@ -28,12 +19,24 @@ const ListingListPreview = ({ booking }) => {
         </b>
         <b>{' '}{booking.get('volunteer') && priceFormat(0, booking.getIn(['price', 'cur']))}</b>
       </p>
-      <p>{ __('bookings_id') } <b>{booking.get('_id')}</b></p>
-      { booking.get('description') &&
-        <p>{ booking.get('description').slice(0, 120) }{ booking.get('description').length > 120 && '...' }</p>
-      }
+      <p>
+        {__('bookings_id')} <b>{bookingId}</b>
+      </p>
+      {booking.get('description') && (
+        <p>
+          {booking.get('description').slice(0, 120)}
+          {booking.get('description').length > 120 && '...'}
+        </p>
+      )}
+      <div className="my-2">
+        <Link passHref href={`/bookings/${bookingId}/cancel`}>
+          <a className="btn">
+            {__('booking_cancel_button')}
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
 
-export default ListingListPreview;
+export default BookingListPreview;
