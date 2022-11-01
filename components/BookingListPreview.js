8062fbfd1dcbb92e73c1415d1914714214620f6c
Vladislav Sorokin <sorokinvj@gmail.com>
2022-11-01 16:41:13 +0000

    make cancel button disabled if now is after booking start

--- components/BookingListPreview.js
+++ components/BookingListPreview.js
@@ -5,9 +5,12 @@ import dayjs from 'dayjs';
 import { __, priceFormat } from '../utils/helpers';
 
 const BookingListPreview = ({ booking }) => {
+  const bookingId = booking.get('_id')
+  const now = dayjs();
   const start = dayjs(booking.get('start'));
   const end = dayjs(booking.get('end'));
-  const bookingId = booking.get('_id')
+  const isBookingCancelable = now.isBefore(start);
+
   return (
     <div className="booking-list-preview card">
       <p>{ __('bookings_status') } <b>{booking.get('status')}</b></p>
@@ -30,8 +33,10 @@ const BookingListPreview = ({ booking }) => {
       )}
       <div className="my-2">
         <Link passHref href={`/bookings/${bookingId}/cancel`}>
-          <a className="btn">
-            {__('booking_cancel_button')}
+          <a>
+            <button disabled={!isBookingCancelable} className="btn disabled:text-gray-400 disabled:border-gray-400 disabled:cursor-not-allowed">
+              {__('booking_cancel_button')}
+            </button>
           </a>
         </Link>
       </div>
